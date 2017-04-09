cb99efdd42e337960dba7ef96edffc092cc6c661
maxximo <marco.roselli@zoho.com>
2017-04-09 11:02:49 +0200

    added homework

---
+++ functionTest.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def functionTest():
+	print("print this text")
+
+functionTest()
---
+++ pyfile.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print("Type your name")
+inputs = input(">>>>>>>")
+outputs = inputs
+def main():
+	print(outputs)
+if __name__ == '__main__' :
+	main()
